b6e814911cbfc3e41d353abe1e1649ddad0ca3f9
nihalpunnappal <nihalmoosakoya@gmail.com>
2026-06-15 01:40:08 +0530

    Updated API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://mehndi-backend-ygia.onrender.com',
+    baseURL: 'https://mehndi-backend-ygia.onrender.com/api',
 });
 
 api.interceptors.request.use(
